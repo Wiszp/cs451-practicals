d5f80beb62bc014e9df8dbcd52017c193c6c337a
Wiszp <jenglishmd@gmail.com>
2021-05-06 17:53:01 -0400

    Finished practical 15

--- p15-kernels.py
+++ p15-kernels.py
@@ -1,3 +1,6 @@
+# Author: Jack English
+# CSCI 451 Practical 15
+# May 6th, 2021
 #%%
 import random
 from shared import bootstrap_accuracy, simple_boxplot
@@ -58,20 +61,38 @@ class ModelInfo:
 
 # TODO: C is the most important value for a SVM.
 #       1/C is how important the model stays small.
-# TODO: RBF Kernel is the best; explore it's 'gamma' parameter.
-
+# Bringing the C value down absolutely tanks the accuracy of the k = poly2 and k = poly3 models
+# Raising it increases accuracy marginally at first, but then the spread begins to increase.
+# Also, as the C value goes up, accuracy and the accuracy ranges become more uniform among the models.
+# This is particularly noticeable if you kick it all the way up something like 100.0
+# TODO: RBF Kernel is the best; explore its 'gamma' parameter.
+# From https://scikit-learn.org/stable/auto_examples/svm/plot_rbf_parameters.html
+#  "Intuitively, the gamma parameter defines how far the influence of a single training example reaches,
+# with low values meaning ‘far’ and high values meaning ‘close’. The gamma parameters can be seen as the
+# inverse of the radius of influence of samples selected by the model as support vectors."
+# A small gamma value is too restrictive, whereas conversely one that is too high will lead to overfitting
 for cfg in configs:
     variants: T.List[ModelInfo] = []
-    for class_weights in [None, "balanced"]:
-        for c_val in [1.0]:
-            svm = SVMClassifier(C=c_val, class_weight=class_weights, **cfg)
-            svm.fit(X_train, y_train)
-            name = "k={}{} C={} {}".format(
-                cfg["kernel"], cfg.get("degree", ""), c_val, class_weights or ""
-            )
-            accuracy = svm.score(X_vali, y_vali)
-            print("{}. score= {:.3}".format(name, accuracy))
-            variants.append(ModelInfo(name, accuracy, svm))
+    for gamma in [0.2, 0.1, 0.01, 0.001, "auto"]:
+        if cfg["kernel"] == "rbf" and gamma != "auto":
+            continue
+        for class_weights in [None, "balanced"]:
+            for c_val in [5.0]:
+                svm = SVMClassifier(
+                    C=c_val, class_weight=class_weights, gamma=gamma, **cfg
+                )
+                svm.fit(X_train, y_train)
+                name = "k={}{} gamma = {} C={} {}".format(
+                    cfg["kernel"],
+                    cfg.get("degree", ""),
+                    # cfg.get("gamma", "auto"), This is what Professor Foley
+                    gamma,
+                    c_val,
+                    class_weights or "",
+                )
+                accuracy = svm.score(X_vali, y_vali)
+                print("{}. score= {:.3}".format(name, accuracy))
+                variants.append(ModelInfo(name, accuracy, svm))
     best = max(variants, key=lambda x: x.accuracy)
     graphs[best.name] = bootstrap_accuracy(best.model, X_vali, y_vali)
 
